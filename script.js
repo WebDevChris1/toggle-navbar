7c999dd1ede899a30f087e7145c54441ff8595c5
danger2123 <sellmysolenyc@gmail.com>
2021-08-01 21:05:25 -0400

    added navbar transition effect

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+// Grab Elements
 const toggleBtn = document.getElementById("toggle-btn");
 const navContainer = document.getElementById("navbar-container");
 const main = document.querySelector("main");
 
+// Toggle Button
 toggleBtn.addEventListener("click", function () {
-  if (navContainer.style.display === "none") {
-    navContainer.style.display = "flex";
+  if (navContainer.style.left === "-25rem") {
+    navContainer.style.left = "0rem";
     main.setAttribute("style", "background-color: rgba(0, 0, 0, .3)");
   } else {
-    navContainer.style.display = "none";
-    main.removeAttribute("style");
+    navContainer.style.left = "-25rem";
+    main.setAttribute("style", "background-color: transparent");
   }
 });
